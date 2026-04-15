8ecf67d9001e99c07d8ed4a1eb567b0441f3370b
BalajiS74 <balaji.sornavel7418@gmail.com>
2026-04-15 15:13:11 +0530

    git-pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
 import React from "react";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./layout/Layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Service from "./pages/Service";
 import GetinTouch from "./components/GetinTouch";
 import ApplyJob from "./components/ApplyJob";
-
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
+        {/* Layout wraps all pages */}
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="service" element={<Service />} />
-          <Route path="GetinTouch" element={<GetinTouch />} />
-          <Route path="ApplyJob" element={<ApplyJob />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/service" element={<Service />} />
+          <Route path="/GetinTouch" element={<GetinTouch />} />
+          <Route path="/ApplyJob" element={<ApplyJob />} />
         </Route>
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
